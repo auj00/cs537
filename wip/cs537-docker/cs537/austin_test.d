cd37b220b7e42e080be6897f5759b64a65300786
Austin James <austinj@rockhopper-01.cs.wisc.edu>
2024-11-14 16:50:12 -0600

    xv6 remote dev csl

--- wip/cs537-docker/cs537/austin_test.d
+++
@@ -1,2 +0,0 @@
-austin_test.o: austin_test.c /usr/include/stdc-predef.h types.h stat.h \
- user.h wmap.h
--- wip/cs537-docker/cs537/poor_protection.d
+++
@@ -1,2 +0,0 @@
-poor_protection.o: poor_protection.c /usr/include/stdc-predef.h types.h \
- stat.h user.h wmap.h
