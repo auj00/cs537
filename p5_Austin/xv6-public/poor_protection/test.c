093a29f9c725d1df616059d20cf3f979ed15a997
Yash Deshpande <yashd@royal-22.cs.wisc.edu>
2024-11-20 15:36:34 -0600

    slipdays.txt & partners.txt file added

--- p5_Austin/xv6-public/poor_protection/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-char *str = "You can't change a character!";
-int main(void) {
-str[1] = 'O';
-printf("%s\n", str);
-return 0;
-}
--- p5_Austin/xv6-public/types.h
+++
@@ -1,7 +0,0 @@
-typedef unsigned int   uint;
-typedef unsigned short ushort;
-typedef unsigned char  uchar;
-typedef uint pde_t;
-
-// p5
-typedef uint pte_t;
